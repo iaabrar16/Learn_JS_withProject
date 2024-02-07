b98ab18b220c3124ee893867cefbba763f106dda
iaabrar16 <kingaabrar@gmail.com>
2024-02-08 01:23:41 +0600

    Fetch API Done

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// *****   Fetch API with example    ********
 
 
+document.getElementById('get_data').addEventListener('click', getData);
 
 
-
+function getData() {
+    fetch('https://api.chucknorris.io/jokes/random')
+        .then(res => res.json())
+        .then(data => { console.log(data); })
+        .catch(err => { console.log(err); })
+}
 
 
 
